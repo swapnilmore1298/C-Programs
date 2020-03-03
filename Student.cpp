6f26aeacd0999ffff1ead03f686c4dea9888478f
divyakapil <32237487+divyakapil@users.noreply.github.com>
2020-03-03 18:39:57 +0530

    Create Student.cpp
    I/P O/P

---
+++ Student.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{  string s;
+string v;
+  cout<<"enter your name:"<<"\n";
+  getline(cin,s);
+  cout<<"Enter your roll no.";
+  cin>>v;
+  cout<<"Name: "<<s<<"\n";
+  cout<<"Roll no: "<<v;
+  return 0;
+}
