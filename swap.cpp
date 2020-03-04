69978ba8fe5404cb1acfdd4334a09847259cadf9
SWAPNIL MORE <31862326+swapnilmore1298@users.noreply.github.com>
2020-03-04 21:00:08 +0530

    swapwithouttemp.cpp
    This is solution to save memory of 3rd variable

--- swap.cpp
+++ swap.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 int main()
 {
-	int v,c,temp,j;
+	//YOU CAN DO THIS WITHOUT USING TEMPORARY VARIABLE
+	int v,c,j;
 	cout<<"Enter first number:"<<"\n";
-	cin>>v;
+	cin>>v; //SUPPOSE V = 10
 	cout<<"Enter second number:"<<"\n";
-	cin>>j;
+	cin>>j; //SUPPOSE J = 05
 	cout<<"Numbers are :"<<v<<j<<"\n";
-	temp=v;
-	v=j;
-	j=temp;
+	v=j+V; // HERE V BECOMES 15
+	j=v-j; // HERE J BECOMES 10
+	v=v-j; // HERE V BECOMES 5 SO SWAPPED 
 	cout<<"swapped numbers are:"<<v<<j;
 	
 }
